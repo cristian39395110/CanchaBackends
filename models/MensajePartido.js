31591960697f24a5824b1528eb40b397010fee02
Cristian <rv98478@gmail.com>
2025-07-10 18:02:07 -0300

    sigo sin a ver probado

--- models/MensajePartido.js
+++ models/MensajePartido.js
@@ -1,4 +1,4 @@
-import { DataTypes } from 'sequelize';
+const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const MensajePartido = sequelize.define('MensajePartido', {
