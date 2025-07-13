e3edfe2c04f52fae3e5255ebfcdd75c3ea268274
Cristian <rv98478@gmail.com>
2025-07-13 14:43:25 -0300

    preparando chat grupal

--- routes/mensajePartido.js
+++ routes/mensajePartido.js
@@ -1,7 +1,7 @@
 //routes mensaje partido
 const express = require('express');
 const router = express.Router();
-const { MensajePartido, Usuario, Suscripcion } = require('../models');
+const { MensajePartido, Usuario, Suscripcion } = require('../models/model');
 const admin = require('../firebase');
 
 
