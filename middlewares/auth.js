40bdd837aa3effef48b8258976dd0ed743535ff1
Cristian <rv98478@gmail.com>
2025-07-12 21:04:19 -0300

    agregando editar perfil

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 function autenticarToken(req, res, next) {
   const token = req.headers.authorization?.split(' ')[1];
   if (!token) return res.sendStatus(401);
