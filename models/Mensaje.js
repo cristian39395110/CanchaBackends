2e96259ddcf8fabadc148f78b80b5ef07a7cbb73
Cristian <rv98478@gmail.com>
2025-07-20 17:10:13 -0300

    probando partido premium

--- models/Mensaje.js
+++ models/Mensaje.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
+
 const Mensaje = sequelize.define('Mensaje', {
   id: {
     type: DataTypes.INTEGER,
@@ -26,12 +27,13 @@ const Mensaje = sequelize.define('Mensaje', {
   leido: {
     type: DataTypes.BOOLEAN,
     allowNull: false,
-    defaultValue: false
+    defaultValue: false,
   },
   frontendId: {
-  type: DataTypes.STRING,
-  allowNull: true, // opcional por compatibilidad
-  unique: true     // ⚠️ importante para evitar duplicados
-}
+    type: DataTypes.STRING,
+    allowNull: true,
+    unique: true, // ⚠️ importante para evitar duplicados
+  },
 });
+
 module.exports = Mensaje;
